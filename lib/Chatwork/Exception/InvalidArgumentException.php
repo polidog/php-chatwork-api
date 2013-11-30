1549521a1369bdabbea71c313b98ce2e1c72f654
polidog <polidogs@gmail.com>
2013-11-30 19:03:24 +0900

    exceptionの実装

---
+++ lib/Chatwork/Exception/InvalidArgumentException.php
@@ -0,0 +1,11 @@
+<?php
+namespace Chatwork\Exception;
+
+/**
+ * Class InvalidArgumentException
+ * @package Chatwork\Exception
+ */
+class InvalidArgumentException extends \InvalidArgumentException
+{
+
+} 
